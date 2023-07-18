0746abff575125fe2c0031aff034d54244848d04
hendrik-kim <hank.dohwankim@gmail.com>
2023-07-18 13:15:55 +0900

    2nd try

---  BestTimeToBuyAndSellStock.cs
+++  BestTimeToBuyAndSellStock.cs
@@ -4,24 +4,23 @@ namespace leetcode
     {
         public int MaxProfit(int[] prices) // prices = [7,1,5,3,6,4]
         {
-            int currProfit = 0;
-            if (prices.Length <= 1) return currProfit;
+            int max = 0;
+            int min = prices[0];
 
-            //traverse prices from back
-            //if(prices[i] - prices[i-j] > currProfit)
-            //currProfit = prices[i] - prices[i-j]
-            for (int i = prices.Length - 1; i >= 0; i--)
+            for (int i = 1; i < prices.Length; i++)
             {
-                for (int j = i - 1; j >= 0; j--)
+                if (min > prices[i])
                 {
-                    if (prices[i] - prices[j] >= currProfit)
-                    {
-                        currProfit = prices[i] - prices[j];
-                    }
+                    min = prices[i];
+                }
+
+                if (prices[i] - min > max)
+                {
+                    max = prices[i] - min;
                 }
             }
 
-            return currProfit;
+            return max;
         }
     }
 }
