f5996583772c2b4c5d47bcccf049ffc6532e0919
Hendrik Kim <hkim@obsglobal.com>
2023-07-20 13:05:02 -0500

    2nd try

--- ClimbStairs.cs
+++ ClimbStairs.cs
@@ -4,13 +4,21 @@ namespace leetcode_daily
     {
         public int ClimbStairs(int n) //4
         {
-            if (n <= 1) return 1;
+            if (n <= 2)
+            {
+                return n;
+            }
 
-            var oneStep = ClimbStairs(n - 1);
-            var twoStep = ClimbStairs(n - 2);
+            int[] steps = new int[n + 1];
+            steps[1] = 1;
+            steps[2] = 2;
 
+            for (int i = 3; i <= n; i++)
+            {
+                steps[i] = steps[i - 1] + steps[i - 2];
+            }
 
-            return oneStep + twoStep;
+            return steps[n];
         }
     }
 }
